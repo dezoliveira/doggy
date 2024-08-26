e633c36c4e9384283410ffe690463fed0df41795
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-26 14:08:24 -0300

    add: lightgaller plugin

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
 
 </script>
 
-<style scoped>
-
+<style>
+@import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lightgallery.css');
+@import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lg-zoom.css');
+@import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lg-video.css');
 </style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,21 +1,56 @@
 <template>
-  <h1>teste</h1>
-  <ul>
-    <li
-      v-for="img in images"
-      :key="img.id"
+  <main>
+    <h1 style="text-align: center;">Gallery</h1>
+    <lightgallery
+      :settings="{ speed: 500, plugins: plugins }"
+      :onInit="onInit"
+      :onBeforeSlide="onBeforeSlide"
+      class="gallery-container"
     >
-      <img :src="img.image.message" />
-      <h2>{{ img.createdAt }}</h2>
-      <h2>{{ img.id }}</h2>
-    </li>
-  </ul>
+      <a
+        v-for="img in images"
+        :key="img.id"
+        class="gallery-item"
+        :data-src="img.image.message"
+        :data-sub-html="renderSub(img)"
+      >
+        <img :src="img.image.message" class="img-responsive"/>
+        <!-- <h2>{{ img.createdAt }}</h2>
+        <h2>{{ img.id }}</h2> -->
+      </a>
+    </lightgallery>
+  </main>
 </template>
 <script>
 import { useGalleryStore } from '@/store/useGalleryStore';
 import { storeToRefs } from 'pinia';
+import Lightgallery from 'lightgallery/vue';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgVideo from 'lightgallery/plugins/video';
 
 export default {  
+  components: {
+    Lightgallery,
+  },
+  data: () => ({
+    plugins: [lgZoom, lgVideo],
+  }),
+  methods: {
+    onInit: () => {
+      console.log('lightGallery has been initialized');
+    },
+    onBeforeSlide: () => {
+      console.log('calling before slide');
+    },
+    renderSub: (image) => {
+      return `
+        <div style="background: red;">
+          <small>Foto adicionada em: ${image.createdAt}</small></br>
+          <label>Criar com Dog API, todos os direitos reservados</label>
+        </div>
+      `
+    }
+  },
   setup() {
     const store = useGalleryStore()
     const { images } = storeToRefs(store)
@@ -28,8 +63,31 @@ export default {
   }
 }
 </script>
+<style lang="css">
+@import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lightgallery.css');
+@import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lg-zoom.css');
+@import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lg-video.css');
 
+.gallery-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
 
-<style>
+body {
+  margin: 0;
+}
+
+.gallery-item {
+  margin: 5px;
+}
+
+.gallery-item img {
+  max-width: 100px;
+  max-height: 100px;
+  min-width: 100px;
+  min-height: 100px;
+}
 
 </style>
