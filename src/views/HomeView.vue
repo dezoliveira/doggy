cc23120787a2a9ca458f587e9f6c37efef67e6fd
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-28 11:45:25 -0300

    add: dog-logo home-bg experiments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,44 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import Button from '@/components/Button.vue';
-import dogImage from '@/assets/images/logo.png'
+import doggyImage from '@/assets/images/logo2.png'
 </script>
 <template>
+  <div class="background">
+
+  </div>
   <main>
     <div class="container">
-      <span style="text-align: center">
-        <img :src="dogImage" width="400px"/>
+      <span style="text-align: center;">
+        <img :src="doggyImage" width="250px" class="logo-img"/>
         <h1>
           Welcome to<strong> Doggy</strong><br/>
           Your <strong>Doggy Gallery</strong> Here
         </h1>
       </span>
-      <div>
-        <RouterLink to="/dogs">
-          <Button text="Doggy Now" btnClass="primary"/>
-        </RouterLink>
-      </div>
+      <RouterLink to="/dogs">
+        <Button text="Doggy Now" btnClass="primary"/>
+      </RouterLink>
     </div>
   </main>
 </template>
 <style scoped>
+  .background {
+    background-image: url('../assets/images/doggy-bg.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    z-index: -1;
+  }
+
+  .logo-img {
+    border-radius: 50%;
+    background-color: black;
+    padding: 15px;
+  }
+
   strong {
     color: brown
   }
